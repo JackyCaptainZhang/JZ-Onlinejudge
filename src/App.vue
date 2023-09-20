82724e432f4bff97b222eadd436a6ac603f2725d
Jacky Zhang <dyzhjch@gmail.com>
2023-09-20 17:40:57 +0800

    Basic fronted template finish. Next switch to the branch "backend".

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,21 @@ import BasicLayout from "./layouts/BasicLayout";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
 import accessEnum from "@/access/accessEnum";
+import { onMounted } from "vue";
 
 const router = useRouter();
 const store = useStore();
+
+/**
+ * todo The global initialise function. All the codes that only runs ONCE at the start can be written here
+ */
+const doInit = () => {
+  console.log("Welcome to Jacky Online Judge System");
+};
+// Call the initialise function
+onMounted(() => {
+  doInit();
+});
 
 //Check if user clicks on the page that needed the permission control (Can be deleted, in case any invalid click)
 router.beforeEach((to, from, next) => {
