3726e39433bb42b4b6f2c9e10a03f4f86350c89b
Jacky Zhang <dyzhjch@gmail.com>
2023-09-22 10:02:12 +0800

    1. Debug connections between fronted and backend finished 2. API file generated automatically by axios and openapi-typescript-codegen

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,21 +1,27 @@
 import store from "@/store/index";
 import { StoreOptions } from "vuex";
 import accessEnum from "@/access/accessEnum";
+import { UserControllerService } from "../../generated";
 
 export default {
   namespaced: true,
   state: () => ({
     loginUser: {
       userName: "Login",
-      //Change this parameter to give user permission
-      userRole: accessEnum.NOT_LOGIN,
     },
   }),
   //todo use store.dispatch to call this action in any other functions
   actions: {
-    getLoginUser({ commit, state }, payload) {
-      //todo change it to the online SQL data fetch
-      commit("updateUser", payload);
+    async getLoginUser({ commit, state }, payload) {
+      const res = await UserControllerService.getLoginUserUsingGet();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: accessEnum.NOT_LOGIN,
+        });
+      }
     },
   },
   mutations: {
